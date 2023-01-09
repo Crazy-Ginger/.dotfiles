c6f4060d6c3ae494c671dd863fab89d4483c1ade
Rebecca Wardle <mattydwardle@gmail.com>
2023-01-09 18:02:05 +0000

    setup Added pl10k to setup file

--- setup.sh
+++ setup.sh
@@ -29,6 +29,7 @@ function zsh_setup {
 function omz_plugins {
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
 
 # NVim setup
